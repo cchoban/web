349836fba6b258532a5692cb0622d8c8ac752d41
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-07-10 17:38:26 +0200

    add is_logged javascript function

--- packages/static/js/uncompiled/app.js
+++ packages/static/js/uncompiled/app.js
@@ -1,20 +1,34 @@
-import VueTimeago from 'vue-timeago'
 window.Vue = require('vue');
-
 window.axios = require('axios');
+import VeeValidator from 'vee-validate';
+import VueTimeago from 'vue-timeago';
+
+
+function is_logged(){
+  axios
+    .get("/islogged")
+    .then(response => {
+      if(response.data.auth){
+        return true
+      }
+    })
+    .catch(err => {
+      this.loading = false;
+      console.log(err);
+    });
+}
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-/**
-* Next, we will create a fresh Vue application instance and attach it to
-* the page. Then, you may begin adding components to this application
-* or customize the JavaScript scaffolding to fit your unique needs.
-*/
+window.logged = is_logged()
+
+
 
 Vue.component('packages-list', require('../components/PackagesList.vue'));
 Vue.component('readMore', require('../components/readMore.vue'));
 Vue.component('package-page', require('../components/singlePage.vue'));
 Vue.component('installCommand', require('../components/installCommandLine.vue'));
 
+Vue.use(VeeValidator)
 Vue.use(VueTimeago, {
     name: 'Timeago', // Component name, `Timeago` by default
     locale: null, // Default locale
