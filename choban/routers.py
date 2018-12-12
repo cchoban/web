62a0db7a1e471479a707e4b5f28796c800ca2a5e
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2018-12-12 15:34:06 +0100

    make it compatible with docker

--- choban/routers.py
+++ choban/routers.py
@@ -4,7 +4,7 @@ from api.viewsets import ArticleViewSet, SubmitPackageViewSet, LoginViewset, Reg
 router = routers.DefaultRouter()
 
 router.register(r'packages', ArticleViewSet, "Packages")
-router.register(r'/token', GetTokenViewset, "GetToken")
-router.register(r'/login', LoginViewset, "LoginAPI")
-router.register(r'/register', RegisterViewset, "RegisterAPI")
-router.register(r'/push', SubmitPackageViewSet, "PushApi")
+router.register(r'token', GetTokenViewset, "GetToken")
+router.register(r'login', LoginViewset, "LoginAPI")
+router.register(r'register', RegisterViewset, "RegisterAPI")
+router.register(r'push', SubmitPackageViewSet, "PushApi")
