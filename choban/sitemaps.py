39361d8275885c6d126ddbf6b2cd9c79fcd49beb
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2019-01-04 16:51:00 +0100

    custom location for sitemaps

--- choban/sitemaps.py
+++ choban/sitemaps.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from api.models import Package
-
+from django.conf import settings
 
 class PackageSitemap(Sitemap):
     changefreq = "weekly"
@@ -11,3 +11,6 @@ class PackageSitemap(Sitemap):
 
     def lastmod(self, obj):
         return obj.updated_at
+
+    def location(self, item):
+        return '/packages/'+str(item)
