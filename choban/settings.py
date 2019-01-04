0fe893628c9b0aef2d55ab76f6bfe4a83df6c3b3
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2019-01-04 15:28:36 +0100

    move read env

--- choban/settings.py
+++ choban/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os, environ
+
 env = environ.Env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 URL = env('URL') or 'https://choban.app'
 
 
