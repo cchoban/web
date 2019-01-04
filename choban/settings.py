c1cb9adee7164f7f6bd6a85fee3de0b5828d1abe
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2019-01-04 15:26:54 +0100

    URL to .env file

--- choban/settings.py
+++ choban/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os, environ
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-URL = "https://choban.herokuapp.com"
+URL = env('URL') or 'https://choban.app'
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
